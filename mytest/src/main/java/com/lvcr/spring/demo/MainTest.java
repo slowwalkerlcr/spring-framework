d92fe870d2a83f01a0b4942c1d01a8ac1544be17
EdisonLv <slowwalkerlcr@163.com>
2020-07-06 22:29:43 +0800

    commit mytest

---
+++ mytest/src/main/java/com/lvcr/spring/demo/MainTest.java
@@ -0,0 +1,16 @@
+package com.lvcr.spring.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @description
+ * @author: changrong_lv
+ * @version: 1.0
+ * @date: 2019/12/31 14:30
+ */
+public class MainTest {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext();
+	}
+
+}
